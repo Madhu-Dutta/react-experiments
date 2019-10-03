64225c2a92194e1a963810d049c5cd0e98c732d2
Madhu Dutta <mdutta787@gmail.com>
2019-10-03 19:09:58 +1000

    Some minor bugfix

--- Day3-StatePropsTest/src/App.js
+++ Day3-StatePropsTest/src/App.js
@@ -10,19 +10,16 @@ function App() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" crossOrigin="anonymous" />
-        <div className="container" id="content">
-          <Main />
-          <Counter />
-        </div>
-        < footer >
-          <Footer />
-        </footer >
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" crossOrigin="anonymous" />
+      <div className="container" id="content">
+        <Main />
+        <Counter />
+      </div>
+      < footer >
+        <Footer />
+      </footer >
     </React.Fragment >
-      );
-    }
-    
-    export default App;
+  );
+}
+
+export default App;
